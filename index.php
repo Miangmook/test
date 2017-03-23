94926cc481497f69e45b7bd2f79240a549c2f9d2
Tests-man <pex_zii@hotmail.com>
2017-03-23 15:17:02 +0700

    ปรับใหม่
    by tests-man

--- index.php
+++ index.php
@@ -1,10 +1,1 @@
-ดหดหฟดหฟดหฟดดดฟ
-]
-
-<div class="col-xs-12">Hellosses World</div>
-<div class="col-xs-12">Helloeese World</div>
-<br></br>
 <div class="col-xs-12">Hello World</div>
-<div class="col-xs-12">Hello World</div>
-<div class="col-xs-12">Hello World</div>
-<diV class="col-xs-12 text-center">TEST HELLO WORD</diV>
